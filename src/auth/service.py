af13f7e3213106748e43fdc79e865d83c96883a7
Camilo Caceres <camiloac97@gmail.com>
2024-08-08 16:28:10 -0500

    feat: add get_current_user

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,12 +1,18 @@
 from sqlalchemy.orm import Session
 from datetime import timedelta, datetime
-from jose import jwt
+from jose import jwt, JWTError
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
 
 from models import User as UserModel
 from schemas import User as UserSchema, UserCreate
 
 SECRET_KEY = "mysecretkey"
 EXPIRE_MINUTES = 60 * 24
+ALGORITHM = "HS256"
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+
 
 # check existing user with same username or email
 async def existing_user(db: Session, username: str, email: str):
@@ -24,4 +30,23 @@ async def create_access_token(id: int, username: str):
     encode = {"sub": username, "id": id}
     expires = datetime.now(datetime.UTC) + timedelta(minutes=EXPIRE_MINUTES)
     encode.update({"exp": expires})
-    return jwt.encode(encode, SECRET_KEY, algorithm="HS256")
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+#get current user from token
+async def get_current_user(db: Session, token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        id: int = payload.get("id")
+        expires: datetime = payload.get("exp")
+        if expires < datetime.now():
+            return None
+        
+        if username is None or id is None:
+            return None
+        db_user =db.query(UserModel).filter(UserModel.id == id).first()
+        return db_user
+    except JWTError:
+        return None
+    
+    
